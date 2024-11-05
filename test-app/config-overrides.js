53cd64cbb78015b91ae15adfcf5f305d29afddc3
nicaroni <nicaroni.macaroni@gmail.com>
2024-11-05 15:24:55 +0200

    Initial commit

---
+++ test-app/config-overrides.js
@@ -0,0 +1,8 @@
+const { override, addWebpackModuleRule } = require("customize-cra");
+
+module.exports = override(
+  addWebpackModuleRule({
+    test: /\.scss$/,
+    use: ["style-loader", "css-loader", "sass-loader"],
+  })
+);
---
+++ test-app/src/App.js
@@ -0,0 +1,15 @@
+import TodoApp from './components/TodoApp';
+
+import './index.scss';
+
+function App() {
+  return (
+    <div>
+    
+      <TodoApp />
+    </div>
+  );
+}
+
+
+export default App;
